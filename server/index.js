2261b3a88919642898dbe0e6d469529846126f69
RaghavendraPatel <61054405+RaghavendraPatel@users.noreply.github.com>
2024-01-09 19:19:17 +0530

    final commit

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const md5 = require('md5')
 
 const cors = require('cors');
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
     optionSuccessStatus: 200
 }
